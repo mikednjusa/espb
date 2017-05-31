f14eeb13ac5eb0a28bf1addf998a4e511220241c
Jocelyn Chang <jrc300@gmail.com>
2017-05-31 00:44:01 -0700

    started updating run test script

--- AWS/ESRally/run_test.py
+++ AWS/ESRally/run_test.py
@@ -22,25 +22,86 @@ def parseArgs():
   return parser.parse_args()
 
 
-def run_single_test(test_suite_name, name, region, instance_type, security_group_ids, test,
-                        root_size_gb, rally_config, save_on_failure=False, debug=True, **kwargs):
-  es_test = ES_Test(test_suite_name=test_suite_name,
-                    name=name,
-                    region=region,
-                    instance_type=instance_type,
-                    security_group_ids=security_group_ids,
-                    test=test,
-                    root_size_gb=root_size_gb,
-                    rally_config=rally_config,
-                    save_on_failure=save_on_failure,
-                    debug=debug)
-  print('running tests...')
-  es_test.run_tests()
+def run_single_test(test, bucket):
+  test_params=json.loads(single_test)
+  output, returncode = run('docker exec -it esrally {0}'.format(test_params['test']), debug=self.debug)
+  if returncode:
+    response = boto3.resource('s3').meta.client.upload_file('logging', bucket, 'log.txt')
+  return
+
+# General purpose run command:
+def run(cmd, hide_command=True, raiseOnFailure=True,
+        retry_count=0,
+        retry_sleep_secs=30, debug=False):
+  try:
+    xrange
+  except NameError:
+    xrange = range
+  for i_attempt in xrange(retry_count + 1):
+    output = None
+    stdout = None
+    stderr = None
+    returncode = None
+    try:
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
+        output = safe_str(p.communicate()[0])
+        returncode = p.returncode
+    except Exception as e:
+      logging.exception(str(e))
+    if returncode != 0:
+        # There was an error, lets retry, if possible:
+        if i_attempt != retry_count:
+            # Only sleep if not end of the loop:
+            logging.exception('retrying command: {}, after sleeping: {}s'.format(cmd, retry_sleep_secs))
+            time.sleep(retry_sleep_secs)
+        continue
+    else:
+        # Command was success, let's not retry:
+        break
+
+  if returncode != 0 and raiseOnFailure is True:
+    logging.exception('***Error in command and raiseOnFailure is True so exiting. CMD:\n{0}'.format(cmd))
+    all_output = output
+
+    logging.exception('This is the output from that command, if any:\n{0}'.format(all_output))
+    raise Exception('Command_Error')
+
+  if debug is True:
+    logging.exception('Debug Information:\noutput:\n{0}\nreturncode: {1}'.format(output, returncode))
+  return output, returncode
+
+  def safe_str(obj):
+        """ return the byte string representation of obj """
+        try:
+            return str(obj)
+        except UnicodeEncodeError:
+            # obj is unicode
+            return unicode(obj).encode('unicode_escape')
+
+  # Setup thread safe logging:
+  def setup_logging(self):
+      self.log = logging.getLogger(self.name)
+      self.log.setLevel(logging.DEBUG)
+
+      # Create the Formater:
+      logFormatter = logging.Formatter("%(asctime)s %(message)s")
+      logFormatter.converter = time.gmtime
+
+      # Write to individual file:
+      fileHandler = logging.FileHandler(self.log_name)
+      fileHandler.setFormatter(logFormatter)
+      self.log.addHandler(fileHandler)
+
+      # And write to console output:
+      consoleHandler = logging.StreamHandler()
+      consoleHandler.setFormatter(logFormatter)
+      self.log.addHandler(consoleHandler)
+
 
 if __name__ == '__main__':
     
   args = parseArgs()
-  data = boto3.resource('s3').meta.client.download_file(args.bucket, 'data_fileljson', '/home/ec2-user/espb/AWS/ESRally/data_file.json')
+  data = boto3.resource('s3').meta.client.download_file(args.bucket, 'data_file.json', '/home/ec2-user/espb/AWS/ESRally/data_file.json')
 
   # Run all tests of a test suite in parallel:
   for test_suite, tests in data['test_suites'].items():
@@ -52,7 +113,7 @@ if __name__ == '__main__':
               continue
           test['test_suite_name'] = test_suite
           test['debug'] = args.debug
-          threads.append(threading.Thread(target=run_single_test, kwargs=test))
+          threads.append(threading.Thread(target=run_single_test, kwargs=[test, args.bucket]))
       # Start running the threads:
       print('starting threads...')
       for t in threads:
@@ -62,3 +123,6 @@ if __name__ == '__main__':
       for t in threads:
           t.join()
   print('Script is done!')
+
+  # tear down stack and benchmarking
+  boto3.client('cloudformation').delete_stack(stackName='ES-Benchmarking-Stack')
