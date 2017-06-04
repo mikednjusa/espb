e30cdc4fef2d2f08bf2815b2d6fd36c77f9d515b
Jocelyn Chang <jrc300@gmail.com>
2017-06-04 02:34:59 -0700

    changed bash commands to docker-py sdk)

--- AWS/ESRally/run_test.py
+++ AWS/ESRally/run_test.py
@@ -2,14 +2,11 @@ import os
 from os import listdir
 from os.path import isfile, join
 import sys
-import threading
+import time
 import argparse
 import json
-import collections
-import requests
 import boto3
-import re
-import subprocess
+import docker
 import zipfile
 
 rootpath = '/home/ec2-user/espb/AWS/'
@@ -28,70 +25,41 @@ def parseArgs():
 
 
 def init_esrally():
-  bashCommand = 'docker exec -it -u root esrally mkdir /home/es/.rally'
-  run(bashCommand)
+  client = docker.from_env()
+  container = client.containers.get('esrally')
 
-  bashCommand = 'docker exec -it -u root esrally chown -R es:es /home/es/.rally'
-  run(bashCommand)
+  cmd = 'mkdir /home/es/.rally'
+  container.exec_run(cmd, tty=True, privileged=False, user='root')
+
+  cmd = 'chown -R es:es /home/es/.rally'
+  container.exec_run(cmd, tty=True, privileged=False, user='root')
   return
 
 
 def run_single_test(test, bucket):
 
-  bashCommand = 'docker cp {0} esrally:/home/es/.rally/rally.ini'.format(test['rally_config'])
-  run(bashCommand)
+  client = docker.from_env()
+  container = client.containers.get('esrally')
 
-  bashCommand = 'docker exec -it -u root esrally chown es:es /home/es/.rally/rally.ini'
-  run(bashCommand)
-
-  bashCommand = 'docker exec -it esrally {0}'.format(test['test'])
-  print bashCommand
-  run(bashCommand)
-
-  bashCommand = 'docker exec -it -u root esrally chmod -R 775 /home/es/.rally/logs'
-  run(bashCommand)
+  cmd = 'mkdir /home/es/.rally'
+  container.exec_run(cmd, tty=True, privileged=False, user='root')
   
-  bashCommand = 'docker cp esrally:/home/es/.rally/logs/. /home/ec2-user/espb/AWS/ESRally/logs'
-  run(bashCommand)
+  #cmd = 'docker cp {0} esrally:/home/es/.rally/rally.ini'.format(test['rally_config'])
+  container.put_archive('/home/es/.rally/rally.ini', test['rally_config'])
+
+  cmd = 'chown es:es /home/es/.rally/rally.ini'
+  container.exec_run(cmd, tty=True, privileged=False, user='root')
+
+  cmd= "test['test']"
+  container.exec_run(cmd, tty=True, privileged=False, user='root')
+
+  cmd = 'chmod -R 775 /home/es/.rally/logs'
+  container.exec_run(cmd, tty=True, privileged=False, user='root')
+  
+  #cmd = 'docker cp esrally:/home/es/.rally/logs/. /home/ec2-user/espb/AWS/ESRally/logs'
+  container.get_archive('/home/es/.rally/logs/.')
 
   return
-
-# General purpose run command:
-def run(cmd, raiseOnFailure=True, retry_count=0, retry_sleep_secs=30):
-    try:
-        xrange
-    except NameError:
-        xrange = range
-    for i_attempt in xrange(retry_count + 1):
-        output = None
-        stdout = None
-        stderr = None
-        returncode = None
-        try:
-            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
-            output = p.communicate()[0]
-            returncode = p.returncode
-        except Exception:
-            print 'Error printing '
-        if returncode != 0:
-            # There was an error, lets retry, if possible:
-            if i_attempt != retry_count:
-                # Only sleep if not end of the loop:
-                print 'Retrying'
-                time.sleep(retry_sleep_secs)
-            continue
-        else:
-            # Command was success, let's not retry:
-            break
-
-    if returncode != 0 and raiseOnFailure is True:
-        print '***Error in command and raiseOnFailure is True so exiting. CMD:\n{0}'.format(cmd)
-        all_output = output
-
-        print 'This is the output from that command, if any:\n{0}'.format(all_output)
-        #raise Exception('Command_Error')
-
-    return output, returncode
 
 
 if __name__ == '__main__':
@@ -100,6 +68,14 @@ if __name__ == '__main__':
   boto3.resource('s3').meta.client.download_file(args.bucket, 'data_file.json', 'data_file.json')
   with open('data_file.json') as json_data:
     data = json.load(json_data)
+
+  client = docker.from_env()
+  for i in range(0,10):
+    if len(client.containers.list()) < 3:
+      time.sleep(30)
+    else:
+      time.sleep(30)
+      break
 
   init_esrally()
 
